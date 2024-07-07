2af1f42b4c79e43282893cb50b986a1d7f0dc306
Nicolas_M <nsoccer344@outlook.com>
2024-07-07 13:27:44 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './Header';
+import Footer from './Footer';
+import NavBar from './NavBar';
+import Home from './Home';
+import About from './About';
+import Articles from './Articles';
+import Browse from './Browse';
+import Tutorials from './Tutorials';
+import Forums from './Forums';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <NavBar /> 
+        <div className="content-wrapper">
+          <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/articles" element={<Articles />} />
+          <Route path="/browse" element={<Browse />} />
+          <Route path="/tutorials" element={<Tutorials />} />
+          <Route path="/forums" element={<Forums />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
